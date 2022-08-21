c74254c226b3d4af1eb869a53873b3c5fe4d2e7f
jiahuan_chen <szdxcjh@163.com>
2022-08-21 21:45:40 +0800

    add code

--- DeepFM/Pytorch_DeepFM/model/DeepFM.py
+++ DeepFM/Pytorch_DeepFM/model/DeepFM.py
@@ -1,36 +1,19 @@
 # -*- coding: utf-8 -*-
-
-"""
-A pytorch implementation of DeepFM for rates prediction problem.
-"""
-
+from nis import cat
+from pkgutil import extend_path
+from turtle import forward
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
 from time import time
 
 
 class DeepFM(nn.Module):
-    """
-    A DeepFM network with RMSE loss for rates prediction problem.
-
-    There are two parts in the architecture of this network: fm part for low
-    order interactions of features and deep part for higher order. In this 
-    network, we use bachnorm and dropout technology for all hidden layers,
-    and "Adam" method for optimazation.
-
-    You may find more details in this paper:
-    DeepFM: A Factorization-Machine based Neural Network for CTR Prediction,
-    Huifeng Guo, Ruiming Tang, Yunming Yey, Zhenguo Li, Xiuqiang He.
-    """
-
     def __init__(self, feature_sizes, embedding_size=4,
                  hidden_dims=[32, 32], num_classes=10, dropout=[0.5, 0.5], 
                  use_cuda=True, verbose=False):
         """
-        Initialize a new network
 
         Inputs:
         - feature_size: 一个整数列表，给出每个字段特征大小(数据数目)
@@ -49,129 +32,91 @@ class DeepFM(nn.Module):
         self.num_classes = num_classes
         self.dtype = torch.float
 
-        """
-            check if use cuda
-        """
         if use_cuda and torch.cuda.is_available():
             self.device = torch.device('cuda')
         else:
             self.device = torch.device('cpu')
+            
         """
-            init fm part
+        数据集中，连续数据过线性层/全连接层，离散数据过embedding层
         """
-        #将离散后的特征
-        fm_first_order_Linears = nn.ModuleList(
-                [nn.Linear(feature_size, self.embedding_size) for feature_size in self.feature_sizes[:13]])
-        fm_first_order_embeddings = nn.ModuleList(
-                [nn.Embedding(feature_size, self.embedding_size) for feature_size in self.feature_sizes[13:40]])
-        self.fm_first_order_models = fm_first_order_Linears.extend(fm_first_order_embeddings)
-
-        fm_second_order_Linears = nn.ModuleList(
-                [nn.Linear(feature_size, self.embedding_size) for feature_size in self.feature_sizes[:13]])
-        fm_second_order_embeddings = nn.ModuleList(
-                [nn.Embedding(feature_size, self.embedding_size) for feature_size in self.feature_sizes[13:40]])
-        self.fm_second_order_models = fm_second_order_Linears.extend(fm_second_order_embeddings)
-
+        first_dense_embedding = nn.ModuleList(
+            [nn.Linear(size,self.embedding_size) for size in self.feature_sizes[:13]]
+        )
+        first_sparse_embedding = nn.ModuleList(
+            [nn.Embedding(size,self.embedding_size) for size in self.feature_sizes[13:40]]
+        )
+        self.first_embedding = first_dense_embedding.extend(first_sparse_embedding)
+        
+        second_dense_embedding = nn.ModuleList(
+            [nn.Linear(size,self.embedding_size) for size in self.feature_sizes[:13]]
+        )
+        second_sparse_embedding = nn.ModuleList(
+            [nn.Embedding(size,self.embedding_size) for size in self.feature_sizes[13:40]]
+        )
+        self.second_embedding = second_dense_embedding.extend(second_sparse_embedding)
+        
         """
-            init deep part
+        深度部分
         """
         
-        all_dims = [self.field_size * self.embedding_size] + \
-            self.hidden_dims + [self.num_classes]
-        #1-2层，每层设置参数
+        all_dims = [self.field_size*self.embedding_size]+self.hidden_dims+[self.num_classes]
+        for i in range(1,len(self.hidden_dims) + 1):
+            setattr(self,'linear'+str(i),nn.Linear(all_dims[i-1],all_dims[i]))
+            setattr(self,'bn'+str(i),nn.BatchNorm1d(all_dims[i]))
+            setattr(self,'drop'+str(i),nn.Dropout(dropout[i-1]))
+        
+    def forward(self,Xi,Xv):
         """
-        nn.Linear(156,32)
-        BatchNorm1d(32)
-        Dropout(0.5)
-        nn.Linear(32,32)
-        BatchNorm1d(32)
-        Dropout(0.5)
+        Args:
+            Xi: 输入位置索引：(N, field_size, 1)
+            Xv: 输入的值：(N, field_size, 1)
         """
-        for i in range(1, len(hidden_dims) + 1):
-            setattr(self, 'linear_'+str(i),
-                    nn.Linear(all_dims[i-1], all_dims[i]))
-            # nn.init.kaiming_normal_(self.fc1.weight)
-            setattr(self, 'batchNorm_' + str(i),
-                    nn.BatchNorm1d(all_dims[i]))
-            setattr(self, 'dropout_'+str(i),
-                    nn.Dropout(dropout[i-1]))
-
-    def forward(self, Xi, Xv):
-        """
-        Forward process of network. 
-
-        Inputs:
-        - Xi: A tensor of input's index, shape of (N, field_size, 1)
-        - Xv: A tensor of input's value, shape of (N, field_size, 1)
-        """
-        """
-            fm part
-        """
-        emb = self.fm_first_order_models[20]
-#        print(Xi.size())
-        for num in Xi[:, 20, :][0]:
-            if num > self.feature_sizes[20]:
-                print("index out")
-
-#        fm_first_order_emb_arr = [(torch.sum(emb(Xi[:, i, :]), 1).t() * Xv[:, i]).t() for i, emb in enumerate(self.fm_first_order_models)]
-#        fm_first_order_emb_arr = [(emb(Xi[:, i, :]) * Xv[:, i])  for i, emb in enumerate(self.fm_first_order_models)]
-        fm_first_order_emb_arr = []
-        for i, emb in enumerate(self.fm_first_order_models):
-            if i <=12:
-                Xi_tem = Xi[:, i, :].to(device=self.device, dtype=torch.float)
-                fm_first_order_emb_arr.append((torch.sum(emb(Xi_tem).unsqueeze(1), 1).t() * Xv[:, i]).t())
+        #拿个系数
+        fm_first_emb = []
+        for i,emb in enumerate(self.first_embedding):
+            if i<13:
+                Xi_ = Xi[:, i, :].to(device=self.device, dtype=torch.float)
+                fm_first_emb.append((emb(Xi_).t() * Xv[:, i]).t())
             else:
-                Xi_tem = Xi[:, i, :].to(device=self.device, dtype=torch.long)
-                fm_first_order_emb_arr.append((torch.sum(emb(Xi_tem), 1).t() * Xv[:, i]).t())
-#        print("successful")      
-#        print(len(fm_first_order_emb_arr))
-        fm_first_order = torch.cat(fm_first_order_emb_arr, 1)
-        # use 2xy = (x+y)^2 - x^2 - y^2 reduce calculation
-#        fm_second_order_emb_arr = [(torch.sum(emb(Xi[:, i, :]), 1).t() * Xv[:, i]).t() for i, emb in enumerate(self.fm_second_order_models)]
-        # fm_second_order_emb_arr = [(emb(Xi[:, i]) * Xv[:, i]) for i, emb in enumerate(self.fm_second_order_embeddings)]
-        fm_second_order_emb_arr = []
-        for i, emb in enumerate(self.fm_second_order_models):
-            if i <=12:
-                Xi_tem = Xi[:, i, :].to(device=self.device, dtype=torch.float)
-                fm_second_order_emb_arr.append((torch.sum(emb(Xi_tem).unsqueeze(1), 1).t() * Xv[:, i]).t())
+                Xi_ = Xi[:, i, :].to(device=self.device, dtype=torch.long)
+                fm_first_emb.append((torch.sum(emb(Xi_),1).t() * Xv[:, i]).t())
+        #特征集合
+        fm_first_emb_all = torch.cat(fm_first_emb, 1)
+        fm_second_emb = []
+        for i,emb in enumerate(self.second_embedding):
+            if i<13:
+                Xi_ = Xi[:, i, :].to(device=self.device, dtype=torch.float)
+                fm_second_emb.append((emb(Xi_).t() * Xv[:, i]).t())
             else:
-                Xi_tem = Xi[:, i, :].to(device=self.device, dtype=torch.long)
-                fm_second_order_emb_arr.append((torch.sum(emb(Xi_tem), 1).t() * Xv[:, i]).t())
-                
-        fm_sum_second_order_emb = sum(fm_second_order_emb_arr)
-        fm_sum_second_order_emb_square = fm_sum_second_order_emb * \
-            fm_sum_second_order_emb  # (x+y)^2
-        fm_second_order_emb_square = [
-            item*item for item in fm_second_order_emb_arr]
-        fm_second_order_emb_square_sum = sum(
-            fm_second_order_emb_square)  # x^2+y^2
+                Xi_ = Xi[:, i, :].to(device=self.device, dtype=torch.long)
+                fm_second_emb.append((torch.sum(emb(Xi_),1).t() * Xv[:, i]).t())
+        #xy=0.5*[(x+y)^(2)-(x^2+y^2)]
+        fm_second_emb_all = sum(fm_second_emb)
+        #(x+y)^(2)
+        fm_sum_second_order_emb_square = fm_second_emb_all*fm_second_emb_all
+        #x^2+y^2
+        fm_sum_second_order_emb = [item*item for item in fm_second_emb]
+        fm_second_order_emb_square_sum = sum(fm_sum_second_order_emb)
         fm_second_order = (fm_sum_second_order_emb_square -
                            fm_second_order_emb_square_sum) * 0.5
-        """
-            deep part
-        """
-#        print(len(fm_second_order_emb_arr))
-#        print(torch.cat(fm_second_order_emb_arr, 1).shape)
-        deep_emb = torch.cat(fm_second_order_emb_arr, 1)
+        
+        """deep"""
+        deep_emb = torch.cat(fm_second_emb,1)
         deep_out = deep_emb
         for i in range(1, len(self.hidden_dims) + 1):
-            deep_out = getattr(self, 'linear_' + str(i))(deep_out)
-            deep_out = getattr(self, 'batchNorm_' + str(i))(deep_out)
-            deep_out = getattr(self, 'dropout_' + str(i))(deep_out)
-#            print("successful") 
-        """
-            sum
-        """
-#        print("1",torch.sum(fm_first_order, 1).shape)
-#        print("2",torch.sum(fm_second_order, 1).shape)
-#        print("deep",torch.sum(deep_out, 1).shape)
-#        print("bias",bias.shape)
+            deep_out = getattr(self,'linear'+str(i))(deep_out)
+            deep_out = getattr(self,'bn'+str(i))(deep_out)
+            if(i != len(self.hidden_dims)):
+                deep_out = getattr(self,'drop'+str(i))(deep_out)
+                
+        #三部分求和
         bias = torch.nn.Parameter(torch.randn(Xi.size(0)))
-        total_sum = torch.sum(fm_first_order, 1) + \
+        total_sum = torch.sum(fm_first_emb_all, 1) + \
                     torch.sum(fm_second_order, 1) + \
                     torch.sum(deep_out, 1) + bias
+                    
         return total_sum
-
     def fit(self, loader_train, loader_val, optimizer, epochs=1, verbose=False, print_every=5):
         """
         Training a model and valid accuracy.
@@ -179,13 +124,10 @@ class DeepFM(nn.Module):
         Inputs:
         - loader_train: I
         - loader_val: .
-        - optimizer: Abstraction of optimizer used in training process, e.g., "torch.optim.Adam()""torch.optim.SGD()".
-        - epochs: Integer, number of epochs.
-        - verbose: Bool, if print.
-        - print_every: Integer, print after every number of iterations. 
-        """
-        """
-            load input data
+        - optimizer: 优化器 e.g., "torch.optim.Adam()""torch.optim.SGD()".
+        - epochs: .
+        - verbose: 是否输出
+        - print_every: 每次迭代后打印
         """
         model = self.train().to(device=self.device)
         criterion = F.binary_cross_entropy_with_logits
@@ -197,8 +139,6 @@ class DeepFM(nn.Module):
                 y = y.to(device=self.device, dtype=self.dtype)
                 
                 total = model(xi, xv)
-#                print(total.shape)
-#                print(y.shape)
                 loss = criterion(total, y)
                 optimizer.zero_grad()
                 loss.backward()
@@ -207,7 +147,6 @@ class DeepFM(nn.Module):
                 if verbose and t % print_every == 0:
                     print('Epoch %d Iteration %d, loss = %.4f' % (epoch, t, loss.item()))
                     self.check_accuracy(loader_val, model)
-                    print()
     
     def check_accuracy(self, loader, model):
         if loader.dataset.train:
@@ -224,16 +163,7 @@ class DeepFM(nn.Module):
                 y = y.to(device=self.device, dtype=self.dtype)
                 total = model(xi, xv)
                 preds = (F.sigmoid(total) > 0.5).to(dtype=self.dtype)
-#                print(preds.dtype)
-#                print(y.dtype)
-#                print(preds.eq(y).cpu().sum())
                 num_correct += (preds == y).sum()
                 num_samples += preds.size(0)
-#                print("successful")
             acc = float(num_correct) / num_samples
             print('Got %d / %d correct (%.2f%%)' % (num_correct, num_samples, 100 * acc))
-
-
-
-
-                        
